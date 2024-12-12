88fdc2f07acddef739a57acd5bfcd715b58a3e01
Cieliocas <francielioevangelista@hotmail.com>
2024-12-11 22:38:25 -0300

    metodo map

--- src/arrays.js
+++ src/arrays.js
@@ -8,5 +8,20 @@ redesSociais.forEach(function (redeSocial, indice) {
     console.log(`#${indice} Eu tenho perfil na rede social: ${redeSocial}`);
 });
 
+const alunos = ['Gustavo', 'José', 'Maria', 'João'];
 
+const alunos2 = alunos.map(function (itemAtual) {
+    return {
+        nome: itemAtual,
+        curso: 'Frontend',
+    }
+});
 
+console.log(alunos2);
+
+const numeros = [1, 2, 3, 4, 5];
+const dobroDosNumeros = numeros.map(function (numeroAtual) {
+    return numeroAtual * 2;
+});
+
+console.log(dobroDosNumeros);
