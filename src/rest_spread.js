4073e68cc62ea7c5406a6528b82b90acbf669c0f
Cieliocas <francielioevangelista@hotmail.com>
2024-12-12 03:33:52 -0300

    spread learn

--- src/rest_spread.js
+++ src/rest_spread.js
@@ -1,3 +1,4 @@
+// reset
 function somar(a, b) {
     let soma = 0;
 
@@ -13,3 +14,22 @@ function somar(a, b) {
 }
 
 console.log(somar(10, 20, 30)); // 3
+
+function somarComRest (...args) { // rest operator
+    return args.reduce((acumulador, itemAtual) => acumulador + itemAtual, 0); // 60
+}
+
+console.log(somarComRest(10, 20, 30)); // 60
+
+// spread
+const numeros = [1, 2, 3, 4]
+console.log(...numeros); // 1 2 3 4
+
+const timesDeFutebolDeSP = ['Palmeiras', 'São Paulo', 'Corinthians', 'Santos'];
+const timesDeFutebolDoRJ = ['Flamengo', 'Fluminense', 'Vasco', 'Botafogo'];
+
+const timesDeFutebol = timesDeFutebolDeSP.concat(...timesDeFutebolDoRJ); // altera o array original
+
+// timesDeFutebolDeSP.concat(timesDeFutebolDoRJ); // não altera o array original
+
+console.log(timesDeFutebol);
