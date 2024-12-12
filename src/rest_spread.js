cd76afa043e7782dfd46c4bca8983f195960bb60
Cieliocas <francielioevangelista@hotmail.com>
2024-12-12 03:23:20 -0300

    arguments

--- src/rest_spread.js
+++ src/rest_spread.js
@@ -1,11 +1,13 @@
 function somar(a, b) {
     let soma = 0;
 
-    for (let i = 0; i < arguments.length; i++) {
-        soma += arguments[i];
+    for (let i = 0; i < arguments.length; i++) { // ele não é uma array, mas se comporta como
+        soma += arguments[i]; // arguments é um objeto array
     }
+    // arguments é iterável e pode ser convertido para array
 
-    return soma;
+    return soma; // 60
+    // arguments.map
     // console.log(arguments);
     // return a + b + c;
 }
