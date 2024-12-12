a50392ad42b304512d0986d1c763acfb89318257
Cieliocas <francielioevangelista@hotmail.com>
2024-12-11 23:51:20 -0300

    Arrow para arrays

--- src/arrow.js
+++ src/arrow.js
@@ -2,4 +2,12 @@
 // Arrow function reduzida
 const minhaFuncao = () => "Hello World";
 
+// Arrow function com um parâmetro
+const retornaUmCarro = () => ({
+    marca: "Fiat",
+    modelo: "Uno",
+    ano: 2020
+});
+
 console.log(minhaFuncao());
+console.log(retornaUmCarro());
