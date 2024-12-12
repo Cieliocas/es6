4cc1399cb4e4f418363cb9a225777f5cb5644cbe
Cieliocas <francielioevangelista@hotmail.com>
2024-12-12 00:02:45 -0300

    arrow function e o metodo this

--- src/arrow.js
+++ src/arrow.js
@@ -11,3 +11,19 @@ const retornaUmCarro = () => ({
 
 console.log(minhaFuncao());
 console.log(retornaUmCarro());
+
+const carro = {
+    velocidadeAtual: 40,
+    acelerar: function() { // Função normal tem acesso ao this
+        console.log(this); // this é o objeto carro
+        this.velocidadeAtual += 10;
+    },
+    desacelerar: () => { // Arrow function não tem acesso ao this
+        console.log(this); // this é o objeto global
+        this.velocidadeAtual -= 10; // this é undefined
+    }
+}
+
+carro.acelerar();
+carro.desacelerar();
+console.log(carro.velocidadeAtual);
