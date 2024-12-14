b832d9913e8b23944de2e782c697b761d209fd20
Cieliocas <francielioevangelista@hotmail.com>
2024-12-14 04:06:03 -0300

    teste com instancias e classes

--- src/oo.js
+++ src/oo.js
@@ -26,12 +26,22 @@ class Pokemon { // Função construtora
     }
 }
 
+class Pikachu extends Pokemon { // Função construtora
+    // pikachu é uma classe que extende a classe Pokemon, ou seja ela herda os atributos e métodos da classe Pokemon
+}
+
+const pikachuDoAsh = new Pikachu('Pikachu', 'Elétrico'); // criando um novo objeto com a função construtora
+
  // criando um novo objeto com a função construtora
 const pikachu = new Pokemon('Pikachu', 'Elétrico'); // Mesmo resultado do código comentado abaixo
 // pikachu.nome = 'Pikachu'; // atribuindo valores aos atributos
 // pikachu.tipo = 'Elétrico'; // atribuindo valores aos atributos
-pikachu.atacar('Choque do Trovão'); // chamando o método atacar
+// pikachu.atacar('Choque do Trovão'); // chamando o método atacar
 console.log(pikachu); 
 // é uma classe e não uma instância pois não tem o new
 // o ash tem um pikachu e outra pessoa pode ter outro pikachu
 // a alusão é que o pikachu é uma classe e o pikachu do Ash é uma instância da classe
+
+console.log(pikachuDoAsh); // exibindo o objeto
+
+console.log(pikachuDoAsh instanceof Pikachu); // true
