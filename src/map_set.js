21158ca593d1c70d7939e1db00cced0da71e405f
Cieliocas <francielioevangelista@hotmail.com>
2024-12-14 02:42:12 -0300

    map learn

--- src/map_set.js
+++ src/map_set.js
@@ -1,10 +1,10 @@
 // este arquivo é destinado a apresentar exemplos de uso de map e set
 
-let meuMap = new Map();
-meuMap.set('nome', 'cielio');
+let meuMap = new Map(); // objeto map
+meuMap.set('nome', 'cielio'); // chave, valor
 
-console.log(meuMap);
+console.log(meuMap); // Map { 'nome' => 'cielio' }
 
-const nome = meuMap.get('nome');
+const nome = meuMap.get('nome'); // cielio
 
 console.log(nome);
