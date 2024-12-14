0298da9ff2e750b7561690c4fe78a2e0be941068
Cieliocas <francielioevangelista@hotmail.com>
2024-12-14 02:54:08 -0300

    keys e values nos maps

--- src/map_set.js
+++ src/map_set.js
@@ -2,6 +2,8 @@
 
 let meuMap = new Map(); // objeto map
 meuMap.set('nome', 'cielio'); // chave, valor
+meuMap.set('stack', 'html, css, js'); // chave, valor
+
 
 console.log(meuMap); // Map { 'nome' => 'cielio' } // chave, valor
 
@@ -15,4 +17,16 @@ console.log(meuMap.size); // 1 // imprime o tamanho do map
 
 // meuMap.has('nome'); // true // verifica se a chave existe
 
-console.log(meuMap.has('nome')); // imprime valor true
+console.log(meuMap.has('sobrenome')); // imprime valor true
+
+// meuMap.clear(); // limpa o map
+
+console.log(meuMap.size); // 0 // imprime o tamanho do map após limpeza
+
+for (let chave of meuMap.keys()) {
+    console.log(chave); // imprime a chave // iterando sobre as chaves
+}
+
+for (let valor of meuMap.values()) {
+    console.log(valor); // imprime o valor // iterando sobre os valores
+}
