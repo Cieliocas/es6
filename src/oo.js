9a730976bca9eac6ecae32dd5e139e5509652fa4
Cieliocas <francielioevangelista@hotmail.com>
2024-12-14 03:48:14 -0300

    classe criada (funcao construtora)

--- src/oo.js
+++ src/oo.js
@@ -1,6 +1,9 @@
-function Pokemon(nomeDoPokemon, tipoDoPokemon) {
-    this.nome = nomeDoPokemon;
-    this.tipo = tipoDoPokemon;
+function Pokemon(nomeDoPokemon, tipoDoPokemon) { // Função construtora
+    this.nome = nomeDoPokemon; // this é utilizado para referenciar o objeto que será criado
+    this.tipo = tipoDoPokemon; // atributos do objeto
 }
 
-const pikachu = new Pokemon('Pikachu', 'Elétrico');
+const pikachu = new Pokemon('Pikachu', 'Elétrico'); 
+// criando um novo objeto com a função construtora
+
+
