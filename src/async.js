91e19a5c2efe6a3d9f86d5cad0dd39da7bcd3d46
Cieliocas <francielioevangelista@hotmail.com>
2024-12-14 03:34:42 -0300

    testando promise em async e await

--- src/async.js
+++ src/async.js
@@ -11,7 +11,7 @@ const funcaoMuitoPesadaPromise = new Promise((resolve, reject) => { // resolve =
     try {
         let execucoes = 0;
         for (let i = 0; i < 1000000000; i++) {
-            execucoess++; // teste de erro com variavel errada
+            execucoes++; // teste de erro com variavel errada
         }
         resolve(execucoes); // sucesso
     } catch (error) {
@@ -25,11 +25,22 @@ const funcaoMuitoPesadaPromise = new Promise((resolve, reject) => { // resolve =
 
 console.log('Iniciando a função muito pesada');
 // console.log(funcaoMuitoPesada());
-funcaoMuitoPesadaPromise.then(resultado => console.log(resultado)).catch(erro => console.log(erro)); // sucesso
+// funcaoMuitoPesadaPromise.then(resultado => console.log(resultado)).catch(erro => console.log(erro));
 // o then é chamado quando a promise é resolvida
 // o catch é chamado quando a promise é rejeitada
 console.log('Função muito pesada finalizada');
 
+// o código acima é assíncrono, ou seja, ele não é executado de forma sequencial
+
+async function execucaoPrincipal() {
+    console.log('inicio')
+    // await funcaoMuitoPesadaPromise.then(resultado => console.log(resultado)).catch(erro => console.log(erro));
+    const resultado = await funcaoMuitoPesadaPromise;
+
+    console.log('fim')
+}
+
+execucaoPrincipal();
 
 
 
