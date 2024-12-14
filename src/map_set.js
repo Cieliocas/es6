2c50b2d7d49fff4b8b57a0e3cda8762f3547b803
Cieliocas <francielioevangelista@hotmail.com>
2024-12-14 02:46:45 -0300

    map learn

--- src/map_set.js
+++ src/map_set.js
@@ -3,8 +3,16 @@
 let meuMap = new Map(); // objeto map
 meuMap.set('nome', 'cielio'); // chave, valor
 
-console.log(meuMap); // Map { 'nome' => 'cielio' }
+console.log(meuMap); // Map { 'nome' => 'cielio' } // chave, valor
 
-const nome = meuMap.get('nome'); // cielio
+const nome = meuMap.get('nome'); // cielio // pega o valor da chave
 
-console.log(nome);
+console.log(nome); // cielio
+
+// meuMap.size; // 1 // tamanho do map
+
+console.log(meuMap.size); // 1 // imprime o tamanho do map
+
+// meuMap.has('nome'); // true // verifica se a chave existe
+
+console.log(meuMap.has('nome')); // imprime valor true
